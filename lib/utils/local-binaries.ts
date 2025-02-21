bacd0ccafeb1dde97ad2c29f957b8d1013c68bb3
selen <rustama@maxsol.id>
2025-02-22 01:45:26 +0800

    chore(update): add module list

--- lib/utils/local-binaries.ts
+++ lib/utils/local-binaries.ts
@@ -2,7 +2,7 @@ import { existsSync } from 'fs';
 import { join, posix } from 'path';
 import { CommandLoader } from '../../commands';
 
-const localBinPathSegments = [process.cwd(), 'node_modules', '@nestjs', 'cli'];
+const localBinPathSegments = [process.cwd(), 'node_modules', '@maxsol', 'coma-cli'];
 
 export function localBinExists() {
   return existsSync(join(...localBinPathSegments));
